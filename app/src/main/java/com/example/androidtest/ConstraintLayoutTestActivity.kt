20a9f874af90f691530516dce5d7fefd39178441
Ryota Wakizaka <rwakizaka7@yahoo.co.jp>
2022-07-06 23:59:23 +0900

    a-1-1-2.列配置(LinearLayout)の入れ子

---
+++ app/src/main/java/com/example/androidtest/ConstraintLayoutTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_test)
+    }
+}
--- app/src/main/java/com/example/androidtest/LinearLayoutTestActivity.kt
+++ app/src/main/java/com/example/androidtest/LinearLayoutTestActivity.kt
@@ -1,11 +1,11 @@
 package com.example.androidtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LinearLayoutTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_liner_layout_test)
     }
 }
