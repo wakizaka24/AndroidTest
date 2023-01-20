f7c89c4eb7b447b6ee1e568340c31661d9b2b0f3
脇坂 亮太 <rwakizaka@techfirm.co.jp>
2023-01-20 13:33:40 +0900

    a-3-1-1(2).Buttonのレイアウトとイベント登録

--- app/src/main/java/com/example/androidtest/ButtonTestActivity.kt
+++ app/src/main/java/com/example/androidtest/ButtonTestActivity.kt
@@ -2,10 +2,29 @@ package com.example.androidtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtest.databinding.ActivityButtonTestBinding
 
 class ButtonTestActivity : AppCompatActivity() {
+    lateinit var binding: ActivityButtonTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_button_test)
+        binding = ActivityButtonTestBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // 5. Buttonの押下時のイベントを登録できるか。
+        // ・可能でした。
+        binding.okButtonSwitchButton.setOnClickListener {
+            binding.okButton.isEnabled = !binding.okButton.isEnabled
+        }
+
+        binding.okButtonEnableButton.setOnClickListener {
+            binding.okButton.isEnabled = true
+        }
+
+        binding.okButtonDisableButton.setOnClickListener {
+            binding.okButton.isEnabled = false
+        }
     }
 }
