29b0b99a2413128b84363e242062ad9fd6e6bb72
Ryota Wakizaka <rwakizaka@techfirm.co.jp>
2024-03-08 17:32:20 +0900

    a-5-1-2.Realm DBのデータ保存と更新、削除(配列以外)

--- app/src/main/java/com/example/androidtest/RealmTestService.kt
+++ app/src/main/java/com/example/androidtest/RealmTestService.kt
@@ -1,123 +1,81 @@
 package com.example.androidtest
 
-import com.google.gson.GsonBuilder
-import com.google.gson.annotations.SerializedName
+import android.util.Log
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
-import io.realm.kotlin.ext.query
-import io.realm.kotlin.query.RealmResults
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.EmbeddedRealmObject
+import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
-import retrofit2.Retrofit
-import retrofit2.Call
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
-import java.io.Serializable
-import java.util.Date
+import io.realm.kotlin.types.annotations.PrimaryKey
 
-class RealmTestService {
-    interface APIInterface {
-        @GET("api2_get_test1_table.php")
-        fun getTest1Table(@Query("colum1") colum1: Int?): Call<Api2Response>
-        @POST("api3_post_test1_table.php")
-        fun postTest1Table(@Body body: Api3RequestBody): Call<Void>
-        // GoogleではDeleteメソッドにBodyを持たせるとエラーになる。
-        // DELETEはパスにIDを持たせるらしい。
-        // @DELETE("api4_delete_test1_table.php")
-        // ただ、RFCでは特に禁止していないので持たせる場合は以下の対応を行う。
-        // RFC section 9: Method Definitions
-        @HTTP(method = "DELETE", path = "api4_delete_test1_table.php", hasBody = true)
-        fun deleteTest1Table(@Body body: Api4RequestBody): Call<Void>
-    }
+class Dog: RealmObject {
+    @PrimaryKey
+    var dogId: Int? = null
+    var name: String = ""
+    var age: Int = 0
+}
 
-    data class Api2Response(
-            @SerializedName("data") val data: Api2ResponseData
-        ): Serializable
-    data class Api2ResponseData(
-            @SerializedName("COLUM1") val colum1: Int,
-            @SerializedName("COLUM2") val colum2: String
-        ): Serializable
+class Person: RealmObject {
+    @PrimaryKey
+    var personId: Int? = null
+    var name: String = ""
+    var age: Int = 0
+    var tasks: RealmList<Task> = realmListOf()
+}
 
-    data class Api3RequestBody(
-            @SerializedName("table1_values") val values:
-            Array<Api3RequestTable1Value>
-        ): Serializable {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
+class Task: EmbeddedRealmObject {
+    var name: String = ""
+    var finished: Boolean = false
+}
 
-            other as Api3RequestBody
-
-            if (!values.contentEquals(other.values)) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            return values.contentHashCode()
-        }
-    }
-    data class Api3RequestTable1Value(
-        @SerializedName("COLUM1") val colum1: Int,
-        @SerializedName("COLUM2") val colum2: String
-    ): Serializable
-
-    data class Api4RequestBody(
-        @SerializedName("table1_deleting_keys") val keys:
-        Array<Int>
-    ): Serializable {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Api4RequestBody
-
-            if (!keys.contentEquals(other.keys)) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            return keys.contentHashCode()
-        }
-    }
-
-    companion object {
-        private const val BASE_URL = "https://wakizaka24.sakura.ne.jp/reversi/php/"
-    }
-
-    private var apiInterfaceImpl: APIInterface
-
+object RealmTestService {
+    var realm: Realm
     init {
-        val gsonFactory = GsonConverterFactory.create(
-            GsonBuilder().serializeNulls().create())
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(gsonFactory)
-            .build()
-        apiInterfaceImpl = retrofit.create(APIInterface::class.java)
-    }
+        val config = RealmConfiguration.Builder(
+            schema = setOf(Dog::class, Person::class, Task::class))
+            // 64バイトのキーを動的に作成する必要あり
+            // キーの保存先も安全な場所に保存する必要あり
+            // .encryptionKey(Base64.decode("abc...", Base64.DEFAULT)) // 暗号化キー
+            .initialData {// DBの初期値
+                copyToRealm(Dog().apply {
+                    dogId = 0
+                    name = "いぬ"
+                    age = 3
+                })
+                copyToRealm(Dog().apply {
+                    dogId = 1
+                    name = "うさぎ"
+                    age = 2
+                })
+                copyToRealm(Dog().apply {
+                    dogId = 2
+                    name = "とり"
+                    age = 2
+                })
+                copyToRealm(Person().apply {
+                    personId = 0
+                    name = "りょうたくん"
+                    age = 20
+                })
+                copyToRealm(Person().apply {
+                    personId = 1
+                    name = "新たなりょうたくん"
+                    age = 2
+                    tasks = realmListOf(
+                        Task().apply {
+                            name = "カレンダー作成"
+                            finished = false
+                        },
+                        Task().apply {
+                            name = "アプリ申請"
+                            finished = false
+                        }
+                    )
+                })
+            }.build()
 
-    fun getApiAccess(): APIInterface {
-        return apiInterfaceImpl
+        realm = Realm.open(config)
+        Log.d("Realm情報", "開いたRealm ${realm.configuration.name}")
     }
-
-//    class Event : RealmObject {
-//        var id: String? = null
-//        var title: String? = null
-//        var startDate: Date? = null
-//        var endDate: Date? = null
-//    }
-//
-//    fun getEventList(id: String?): List<Event> {
-//        // 領域を開く
-//        val config = RealmConfiguration.create(schema = setOf(Event::class))
-//        val realm: Realm = Realm.open(config)
-//
-//        val eventList: RealmResults<Event> = realm.query<Event>("id == $id").find()
-//        return eventList.toList()
-//    }
-//
-//    fun addEvent(event: Event) {
-//
-//    }
 }
