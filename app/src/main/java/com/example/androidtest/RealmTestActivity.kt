8b0a97f45baeeda7430a6cfe083147848b86930c
Ryota Wakizaka <rwakizaka@techfirm.co.jp>
2024-04-26 19:02:50 +0900

    a-5-1-3.Realm DBのデータ保存と更新、削除(配列)

--- app/src/main/java/com/example/androidtest/RealmTestActivity.kt
+++ app/src/main/java/com/example/androidtest/RealmTestActivity.kt
@@ -1,47 +1,23 @@
 package com.example.androidtest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtest.databinding.ActivityRealmTestBinding
 import io.realm.kotlin.ext.query
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlin.math.log
 
 class RealmTestActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRealmTestBinding
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRealmTestBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        /*
-        Personの追加(personId=1, エラーハンドリング含む)
-        Personの更新(personId=1, 年齢の更新21歳にする)
-        Personの削除(personId=1)
-        Personのtaskの追加(personId=1, タスクIDを最大+1)
-        Personのtaskの更新(personId=1, タスクIDの最小のレコードをfinished=trueにする)
-        Personのtaskの削除(personId=1 and finished=true)
-         */
-
-
-//        RealmTestService.realm.writeBlocking {
-//            copyToRealm(Dog().apply {
-//                dogId = 0
-//                name = "いぬ"
-//                age = 3
-//            })
-//            copyToRealm(Dog().apply {
-//                dogId = 1
-//                name = "うさぎ"
-//                age = 2
-//            })
-//            copyToRealm(Dog().apply {
-//                dogId = 2
-//                name = "とり"
-//                age = 2
-//            })
-//        }
 
         val dogResult = RealmTestService.realm.query<Dog>(query = "dogId == $0", args = arrayOf(2))
             .find()
@@ -51,30 +27,129 @@ class RealmTestActivity : AppCompatActivity() {
         }
 
         val personNum = RealmTestService.realm.query<Person>().find().size
-        showMessage("初期のDog: $dogName 初期のPersonの数: $personNum")
+        Log.d("Realm情報", "抽出したDog名: $dogName 初期のPersonの数: $personNum")
+        logOfRealm()
+        // showMessage("抽出したDog名: $dogName 初期のPersonの数: $personNum")
 
-//        val tasks = RealmTestService.realm.query<Dog>().find()
-//        CoroutineScope(newSingleThreadContext("RealmMonitoring")).launch {
-//            tasks.asFlow().collect {
-//                when (it) {
-//                    is InitialResults -> {
-//                        showMessage("Dog初期化件数: ${it.list.size}")
-//                    }
-//                    is UpdatedResults -> {
-//                        showMessage("Dog更新件数: ${it.list.size}")
-//                    }
-//                }
-//            }
-//        }
+        binding.addPerson.stateListAnimator = null
+        binding.addPerson.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = this.query<Person>(
+                    query = "personId == $0", args = arrayOf(2)).find()
+                if (personResult.size == 0) {
+                    copyToRealm(Person().apply {
+                        personId = 2
+                        name = "追加した人"
+                        age = 30
+                    })
+                }
+            }
+            logOfRealm()
+        }
+
+        binding.updatePerson.stateListAnimator = null
+        binding.updatePerson.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = this.query<Person>(
+                    query = "personId == $0", args = arrayOf(2)).find()
+                if (personResult.size > 0) {
+                    personResult.first().name = "更新した人"
+                }
+            }
+            logOfRealm()
+        }
+
+        binding.deletePerson.stateListAnimator = null
+        binding.deletePerson.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = this.query<Person>(
+                    query = "personId == $0", args = arrayOf(2)).find()
+                if (personResult.size > 0) {
+                    delete(personResult.first())
+                }
+            }
+            logOfRealm()
+        }
+
+        binding.addPersonTask.stateListAnimator = null
+        binding.addPersonTask.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = this.query<Person>(
+                    query = "personId == $0", args = arrayOf(1)).find()
+                var taskId = 0
+                if (personResult.first().tasks.size > 0) {
+                    val maxTask = personResult.first().tasks.maxBy { it.taskId!! }
+                    taskId = maxTask.taskId!! + 1
+                }
+                personResult.first().tasks.add(Task().apply {
+                    this.taskId = taskId
+                    name = "追加したタスク"
+                    finished = false
+                })
+            }
+            logOfRealm()
+        }
+
+        binding.updatePersonTask.stateListAnimator = null
+        binding.updatePersonTask.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = this.query<Person>(
+                    query = "personId == $0", args = arrayOf(1)).find()
+                if (personResult.size > 0 && personResult.first().tasks.size > 0) {
+                    val minTask = personResult.first().tasks.minBy { it.taskId!! }
+                    minTask.finished = true
+                }
+            }
+            logOfRealm()
+        }
+
+        binding.deletePersonTask.stateListAnimator = null
+        binding.deletePersonTask.setOnClickListener {
+            RealmTestService.realm.writeBlocking {
+                val personResult = query<Person>(
+                    query = "personId == $0", args = arrayOf(1)).find()
+                if (personResult.size > 0) {
+                    val tasks = personResult.first().tasks.filter { it.finished }
+                    personResult.first().tasks.removeAll(tasks)
+                }
+            }
+            logOfRealm()
+        }
     }
 
-    private fun showMessage(message: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Realmの検証")
-            .setMessage(message)
-            .setPositiveButton("OK") { _, _ ->
+//    private fun showMessage(message: String) {
+//        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("Realmの検証")
+//            .setMessage(message)
+//            .setPositiveButton("OK") { _, _ ->
+//            }
+//        builder.create()
+//        builder.show()
+//    }
+
+    private fun logOfRealm() {
+        val parsonResult = RealmTestService.realm.query<Person>().find()
+        Log.d("Realm情報", "---Person---")
+        for (i in 0 ..< parsonResult.size) {
+            val parson = parsonResult[i]
+            Log.d("Realm情報",
+                "${parson.personId} ${parson.name} ${parson.age}")
+            if (parson.tasks.size > 0) {
+                Log.d("Realm情報", "---Person>Task---")
             }
-        builder.create()
-        builder.show()
+            for (j in 0..<parson.tasks.size) {
+                val task = parson.tasks[j]
+                Log.d("Realm情報",
+                    "${task.taskId} ${task.name} ${task.finished}")
+            }
+        }
+
+        Log.d("Realm情報", "---Dog---")
+        val dogResult = RealmTestService.realm.query<Dog>().find()
+        for(i in 0 ..< dogResult.size) {
+            val dog = dogResult[i]
+            Log.d("Realm情報",
+                "${dog.dogId} ${dog.name} ${dog.age}")
+        }
     }
 }
